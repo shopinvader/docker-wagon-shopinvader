53855adbb646d2e194d42c167e19864393258e3a
Sébastien BEAU <sebastien.beau@akretion.com>
2019-04-16 16:57:09 +0200

    [FIX] fix bump script

--- bump.sh
+++ bump.sh
@@ -1,7 +1,9 @@
-#rm Gemfile.lock
-#docky run bundle install
+rm Gemfile.lock
+docky run bundle install
 git add Gemfile Gemfile.lock
 TAG="v4.0.x-`date +%Y%m%d`"
 MESSAGE="Automatic release $TAG"
 git commit -m "$MESSAGE"
 git tag -a "$TAG" -m "$MESSAGE"
+echo "bump done, check result the run"
+echo "git push origin master --tag"
